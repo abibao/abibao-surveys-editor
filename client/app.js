08e111e1573df8397f48901c2fb4a57a7fcc2d84
Gilles Perreymond <gperreymond@viadeoteam.com>
2017-01-13 17:59:12 +0100

    so far so good

--- client/app.js
+++ client/app.js
@@ -1,7 +1,16 @@
 /*global SurveyEditor*/
 'use strict'
 
+// vendors
+import 'bootstrap/dist/js/bootstrap.js'
+import 'bootstrap/dist/css/bootstrap.css'
+
+// modules
 import 'survey-knockout/dist/survey.ko.js'
+import 'surveyjs-editor/dist/surveyeditor.css'
+
+// abibao
+import './styles/style.css'
 
 const editorOptions = {showEmbededSurveyTab: true}
 const editor = new SurveyEditor.SurveyEditor('surveyEditorContainer', editorOptions)
--- webpack.config.js
+++ webpack.config.js
@@ -14,11 +14,15 @@ module.exports = {
   resolve: ['', '.js', '.tag', 'css', 'png'],
   output: {
     path: buildPath,
-    filename: 'js/bundle.js'
+    filename: '/js/bundle.js'
   },
   plugins: [
     new webpack.ProvidePlugin({
-      SurveyEditor: 'surveyjs-editor/dist/surveyeditor'
+      jQuery: 'jquery',
+      $: 'jquery',
+      jquery: 'jquery',
+      ko: 'knockout',
+      SurveyEditor: 'surveyjs-editor'
     }),
     new CopyWebpackPlugin([
       { from: contextPath + '/index.html', to: buildPath + '/index.html' }
@@ -30,9 +34,12 @@ module.exports = {
       { test: /\.tag$/, exclude: /node_modules/, loader: 'riotjs-loader', query: { type: 'babel' } }
     ],
     loaders: [
-      { test: /\.js|\.tag$/, exclude: /node_modules/, include: /client/, loader: 'babel-loader', query: {cacheDirectory: true, presets: ['es2015']} },
+      { test: /\.js|\.tag$/, exclude: /node_modules/, include: path.join(contextPath), loader: 'babel-loader', query: {cacheDirectory: true, presets: ['es2015']} },
       { test: /\.scss$/, loaders: 'style-loader!css-loader!sass-loader' },
-      { test: /\.css$/, loader: 'style-loader!css-loader!postcss-loader' }
+      { test: /\.css$/, loader: 'style-loader!css-loader!postcss-loader' },
+      { test: /\.(png|jpg|gif|svg)$/, exclude: /node_modules/, include: /client/, loader: 'url-loader?limit=8192&name=images/[hash].[ext]' },
+      { test: /\.(woff|woff2|ttf|eot)(\?v=[a-z0-9]\.[a-z0-9]\.[a-z0-9])?$/, loader: 'file-loader?name=fonts/[name].[ext]' },
+      { test: /\.(woff|woff2|ttf|eot|svg)(\?sbpmys)?$/, include: /node_modules/, loader: 'file-loader?name=fonts/[name].[ext]' }
     ]
   }
 }
