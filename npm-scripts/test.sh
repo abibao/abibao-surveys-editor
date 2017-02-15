0d87a4e772832b0d9a2183ae576beb0471d7fa21
gperreymond <gperreymond@gmail.com>
2017-02-15 21:15:24 +0100

    add circleci test & build & push on docker hub

---
+++ npm-scripts/test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+# export CODACY_PROJECT_TOKEN=5705316ff8f6499d9373f0f76340bfa5
+rm -rf coverage
+
+npm run test:coverage
+
+# cat coverage/lcov.info | node_modules/.bin/codacy-coverage
