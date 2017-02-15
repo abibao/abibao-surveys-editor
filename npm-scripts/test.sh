7810d48a80f5a6fdc392a348f536a07541983226
gperreymond <gperreymond@gmail.com>
2017-02-15 21:41:46 +0100

    codacy integration and badges

--- npm-scripts/test.sh
+++ npm-scripts/test.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-# export CODACY_PROJECT_TOKEN=5705316ff8f6499d9373f0f76340bfa5
+export CODACY_PROJECT_TOKEN=13fb91d00cde41639fe71eb088cad5a1
 rm -rf coverage
 
 npm run test:coverage
 
-# cat coverage/lcov.info | node_modules/.bin/codacy-coverage
+cat coverage/lcov.info | node_modules/.bin/codacy-coverage
