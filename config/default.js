a82d5f6ce7541ee0c4a9cdb5ed3c7015297319ec
gperreymond <gperreymond@gmail.com>
2017-02-16 17:28:07 +0100

    circle use local postgre database

--- config/default.js
+++ config/default.js
@@ -6,18 +6,18 @@ const nconf = require('nconf')
 nconf.argv().env().file({ file: 'nconf.json' })
 
 module.exports = {
-  host: nconf.get('ABIBAO_SERVICE_HOST') || 'localhost',
+  host: nconf.get('ABIBAO_SERVICE_HOST') || '127.0.0.1',
   port: nconf.get('ABIBAO_SERVICE_PORT') || 8000,
   superu: {
     email: nconf.get('ABIBAO_ADMINISTRATOR_EMAIL') || 'administrator@abibao.com',
     password: nconf.get('ABIBAO_ADMINISTRATOR_PASSWORD') || 'password'
   },
   postgres: {
-    host: nconf.get('ABIBAO_POSTGRES_HOST') || 'localhost',
+    host: nconf.get('ABIBAO_POSTGRES_HOST') || '127.0.0.1',
     port: nconf.get('ABIBAO_POSTGRES_PORT') || 5432,
-    database: nconf.get('ABIBAO_POSTGRES_DB') || 'abibao',
-    username: nconf.get('ABIBAO_POSTGRES_USER') || 'abibao',
-    password: nconf.get('ABIBAO_POSTGRES_PASSWORD') || 'abibao'
+    database: nconf.get('ABIBAO_POSTGRES_DB') || 'circle_test',
+    username: nconf.get('ABIBAO_POSTGRES_USER') || 'ubuntu',
+    password: nconf.get('ABIBAO_POSTGRES_PASSWORD') || ''
   },
   public: path.resolve(__dirname, '../build'),
   corsWhitelist: ['localhost'],
