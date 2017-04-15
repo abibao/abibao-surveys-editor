c7f7b7b8fdca6ec9cb7fffff7c91f28b691e2c70
Gilles Perreymond <gperreymond@gmail.com>
2017-04-15 19:34:22 +0200

    add logstash to answerGetRandomEHOPAnswerQuery

--- server/services/domain/queries/answerGetRandomEHOPAnswerQuery.js
+++ server/services/domain/queries/answerGetRandomEHOPAnswerQuery.js
@@ -4,6 +4,7 @@ const Promise = require('bluebird')
 const Sequelize = require('sequelize')
 const auth = require('feathers-authentication')
 const permissions = require('feathers-permissions')
+const eraro = require('eraro')({package:'platform.abibao.com'})
 
 const AnswerModel = require('./../../data/answers/model')
 
@@ -21,6 +22,7 @@ class Service {
   }
   get (params) {
     const app = this.app
+    const starttime = new Date()
     const Answer = AnswerModel(app)
     return Answer.findAll({
       where: { 'campaign_id': params.campaign, question: 'AGENCEDURABLE_EHOP_IDEA', individual: { $not: params.individual } },
@@ -41,19 +43,33 @@ class Service {
           offset, limit: 1
         })
       }
-    }).then(Promise.resolve).catch(Promise.reject)
+    })
+    .then((result) => {
+      const endtime = new Date()
+      app.info({
+        env: app.get('env'),
+        exectime: endtime - starttime,
+        type: 'query',
+        name: 'answerGetRandomEHOPAnswer',
+        params
+      })
+      return Promise.resolve(result)
+    })
+    .catch((error) => {
+      const endtime = new Date()
+      app.error({
+        env: app.get('env'),
+        exectime: endtime - starttime,
+        type: 'query',
+        name: 'answerGetRandomEHOPAnswer',
+        error
+      })
+      return Promise.reject(error)
+    })
   }
 }
 
 module.exports = function () {
   const app = this
   app.use('query/answerGetRandomEHOPAnswer', new Service())
-  /* const service = app.service('query/answerGetRandom')
-  service.before({
-    get: [
-      auth.hooks.authenticate('jwt'),
-      permissions.hooks.checkPermissions(options),
-      permissions.hooks.isPermitted()
-    ]
-  }) */
 }
