7ce2f72325d0437dffe62bda6875e1f3b35c8025
Gilles Perreymond <gperreymond@gmail.com>
2017-04-26 23:57:56 +0200

    [docker] couchdb cors added

---
+++ src/pages/admin/libs/Config.js
@@ -0,0 +1,7 @@
+let Config = {
+  hosts: {
+    couchdb: (window.location.host.includes('localhost') ? 'http://infra:infra@localhost:5984' : 'http://abibao:xCEu8rQW9Hnacu6dKu37f46Xm2mPr2@couchdb:5984')
+  }
+}
+
+export default Config
