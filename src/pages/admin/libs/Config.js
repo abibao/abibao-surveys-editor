6b64bfaad73880837f0dbecadc0acc04f6d39347
Gilles Perreymond <gperreymond@gmail.com>
2017-04-30 13:37:10 +0200

    [reader] hotfix onsubmit in form login

--- src/pages/admin/libs/Config.js
+++ src/pages/admin/libs/Config.js
@@ -1,6 +1,29 @@
+let couchdbUser = 'infra'
+let couchdbPass = 'infra'
+
+switch (window.location.hostname) {
+  case 'platform.local.abibao.com':
+    break
+  case 'platform.pprod.abibao.com':
+    couchdbUser = 'rece'
+    couchdbPass = 'rece'
+    break
+  case 'platform.abibao.com':
+    couchdbUser = 'abibao'
+    couchdbPass = 'xCEu8rQW9Hnacu6dKu37f46Xm2mPr2'
+    break
+  default:
+}
+
+let couchdbUrl = '$protocol//$user:$pass@$hostname:5984'
+couchdbUrl = couchdbUrl.replace('$protocol', window.location.protocol)
+couchdbUrl = couchdbUrl.replace('$user', couchdbUser)
+couchdbUrl = couchdbUrl.replace('$pass', couchdbPass)
+couchdbUrl = couchdbUrl.replace('$hostname', window.location.hostname)
+
 let Config = {
   hosts: {
-    couchdb: (window.location.host.includes('localhost') ? 'http://infra:infra@localhost:5984' : 'http://abibao:xCEu8rQW9Hnacu6dKu37f46Xm2mPr2@database.abibao.com:5984')
+    couchdb: couchdbUrl
   }
 }
 
