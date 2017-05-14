e0c720cd4c99cc23a2840d35133d20b88dc1cf9a
Gilles Perreymond <gperreymond@gmail.com>
2017-05-14 14:54:55 +0200

    update tests

--- npm-scripts/test.sh
+++ npm-scripts/test.sh
@@ -5,6 +5,7 @@ set -e
 export CODACY_PROJECT_TOKEN=6b49b03a50254048ba094c28a04d62ab
 rm -rf coverage
 
+npm run test:standard
 npm run test:coverage
 
 cat coverage/lcov.info | node_modules/.bin/codacy-coverage
